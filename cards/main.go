9f43434eef9499d3fe430febbfdd367e64537787
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-05-11 10:49:14 +0530

    Variable Declaration

---
+++ cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Different ways of creating a new var
+	// var card string = "Ace of Spades"
+	// var card = "Ace of Spades"
+	card := "Ace of Spades"
+
+	card = "Six of Diamonds" // Re-assignment
+	fmt.Println(card)
+}
