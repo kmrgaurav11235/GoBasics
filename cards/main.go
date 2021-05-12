8bd53a240c26a84606bdecdcf36d878f9e8ae012
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-05-12 20:01:14 +0530

    Slices in Go

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
+	cards := []string{"Ace of Diamond", newCard()}
 
-	fmt.Println(card)
+	// Append to slice
+	cards = append(cards, "Six of Clubs") // Note that append returns a new slice; it doesn't modifies the original one
+
+	fmt.Println(cards)
 }
 
 func newCard() string {
