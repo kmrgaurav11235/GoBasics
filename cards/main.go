a5ba371a809c74769d505b7090dade807500337d
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-05-12 19:48:31 +0530

    Functions and Return types

--- cards/main.go
+++ cards/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// Different ways of creating a new var
-	// var card string = "Ace of Spades"
-	// var card = "Ace of Spades"
-	card := "Ace of Spades"
+	card := newCard()
 
-	card = "Six of Diamonds" // Re-assignment
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
