22a2277db8e98f7d738094642b6fd6caa98dc711
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-05-20 20:57:35 +0530

    Creating a new Deck

--- cards/deck.go
+++ cards/deck.go
@@ -5,6 +5,22 @@ import "fmt"
 // Create a new type of 'deck' which is a slice of string
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Clubs", "Hearts", "Diamonds"}
+	cardValues := []string{"Ace", "One", "Two", "Three"}
+
+	// "_" tells Go that this value will not be used. Without it, we will get "declared but not used" error.
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	// (d deck) is a Receiver. Any variable of type "deck" now gets access to this "print" method
 	for i, card := range d {
--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamond", newCard()}
-
-	// Append to slice
-	cards = append(cards, "Six of Clubs") // Note that append returns a new slice; it doesn't modifies the original one
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Ace of Spades"
 }
