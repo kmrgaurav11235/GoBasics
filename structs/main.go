e7d9aea9a224732657fe2eac90b38404844b3937
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-05-24 20:26:08 +0530

    Embedding structs

--- structs/main.go
+++ structs/main.go
@@ -2,23 +2,30 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	// paul := person{"Paul", "Atreides"}
-	paul := person{firstName: "Paul", lastName: "Atreides"} // Better way
+	leto := person{
+		firstName: "Leto",
+		lastName:  "Atreides",
+		contact: contactInfo{
+			email:   "leto.da.boss@caladan.gov",
+			zipCode: 560076,
+		},
+	}
+	/*
+		Note that the commas are present at the end of every line above, even when it is the last line.
+		This is required when we define structs using multiple lines.
+	*/
 
-	fmt.Println(paul)
-
-	var leto person            // 'leto' is declared but not assigned a value
-	fmt.Println(leto)          // Will show the default "" value for strings firstName and lastName
-	fmt.Printf("%+v \n", leto) // Prints the field and value
-
-	leto.lastName = "Atreides"
-	leto.firstName = "Leto"
-	fmt.Println(leto)
 	fmt.Printf("%+v \n", leto)
 }
