36e42170e8aa4177c6930f902e008f01956e57af
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-05-24 21:39:56 +0530

    Pointers: pointer type and pointer operators

--- structs/main.go
+++ structs/main.go
@@ -27,14 +27,27 @@ func main() {
 		This is required when we define structs using multiple lines.
 	*/
 
-	leto.updateFitstName("Paul")
-	leto.print() // This will print "Leto" not "Paul"
+	letoPointer := &leto
+	// &variable = Give me the memory address of the value that this variable is pointing at.
+	letoPointer.updateFitstName("Paul")
+	leto.print()
 }
 
-func (p person) updateFitstName(newFirstName string) {
-	// In Go, the 'p person' is passed by value
-	p.firstName = newFirstName
+// Below *person is a type description. It means we are working with a "Pointer to a person".
+func (pointerToPerson *person) updateFitstName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+	// Above *pointer is an operator. It means, "Give me the value at this memory address".
 }
+
+/*
+As we see above "*Type" is completely different from "*PointerVariable"
+	1. *type is a type description.
+	2. *pointer is an operator.
+
+Also, the two operators:
+	1. Turn address into value with "*address"
+	2. Turn value into address with "&value"
+*/
 
 func (p person) print() {
 	fmt.Printf("%+v \n", p)
