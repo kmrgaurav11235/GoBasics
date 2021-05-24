453c6c21693d4203139e34e2e79ff275ceac289e
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-05-24 13:53:40 +0530

    Updating struct values

--- structs/main.go
+++ structs/main.go
@@ -12,4 +12,13 @@ func main() {
 	paul := person{firstName: "Paul", lastName: "Atreides"} // Better way
 
 	fmt.Println(paul)
+
+	var leto person            // 'leto' is declared but not assigned a value
+	fmt.Println(leto)          // Will show the default "" value for strings firstName and lastName
+	fmt.Printf("%+v \n", leto) // Prints the field and value
+
+	leto.lastName = "Atreides"
+	leto.firstName = "Leto"
+	fmt.Println(leto)
+	fmt.Printf("%+v \n", leto)
 }
