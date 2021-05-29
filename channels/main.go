b1b53e543d256f36b7b413f0f3b1a8846ff8ae67
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-05-29 16:24:37 +0530

    Theory of Go Routines

--- channels/main.go
+++ channels/main.go
@@ -15,7 +15,7 @@ func main() {
 	}
 
 	for _, link := range links {
-		checkLink(link)
+		checkLink(link) // starts a new Go Routine to run this function.
 	}
 }
 
@@ -28,3 +28,44 @@ func checkLink(link string) {
 		fmt.Println(link, "is up.")
 	}
 }
+
+/*
+* By default, Go tries to use one core.
+
+				--------------------------
+						One CPU Core
+				--------------------------
+							▼
+	----------------------------------------------------
+						Go Scheduler
+	----------------------------------------------------
+	Go Scheduler runs one routine until it is finished
+	or makes a Blocking call (like an http request).
+	----------------------------------------------------
+		▼					▼					▼
+	----------------------------------------------------
+	Go Routine 1	|	Go Routine 2	|	Go Routine 3
+	----------------------------------------------------
+
+* You change the default setting and ask Go to use multiple cores.
+
+	----------------------------------------------------
+	CPU Core 1		|	CPU Core  2		|	CPU Core 3
+	----------------------------------------------------
+		▼					▼					▼
+	----------------------------------------------------
+						Go Scheduler
+	----------------------------------------------------
+	Go Scheduler runs one routine until it is finished
+	or makes a Blocking call (like an http request).
+	----------------------------------------------------
+		▼					▼					▼
+	----------------------------------------------------
+	Go Routine 1	|	Go Routine 2	|	Go Routine 3
+	----------------------------------------------------
+
+* "Concurrency is not parallelism." - You would hear this quote a lot.
+* Concurrency: We have have multiple threads executing code. If one thread blocks, another one
+	is picked up and worked on.
+* Parallelism: Multiple threads executed at the exact same time. Required multiple CPUs.
+*/
